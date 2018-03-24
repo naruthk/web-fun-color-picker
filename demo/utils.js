0ac40af76f51467e808f835014eb2ab111bf78ff
Naruth Kongurai <contact@naruthk.com>
2018-03-24 02:14:43 -0700

    Rewrote partial code

--- demo/utils.js
+++ demo/utils.js
@@ -1,28 +1,36 @@
-"use strict";
-var numberColRow = 9;
-createGrid(numberColRow);
-function createGrid(amount) {
-    var divGridWrapper = document.getElementById('grid-wrapper');
-    var i = 0;
-    for (i; i < (amount * amount); i++) {if (window.CP.shouldStopExecution(1)){break;}
-        var divTile = document.createElement('div');
-        divTile.classList.add("tiles");
-        divTile.innerText = i;
-        divTile.id = i;
-        divTile.onmouseover = paintBox;
-        divGridWrapper.appendChild(divTile);
-    }
-window.CP.exitedLoop(1);
+// Naruth Kongurai
+// Project: Color Picker
 
+"use strict";
+
+createGrid(9);
+
+// Creates a grid of n amount of rows and columns.
+function createGrid(amount) {
+  const divGridWrapper = document.getElementById('grid-wrapper');
+  const i = 0;
+  for (i; i < amount * amount; i++) {
+    if (window.CP.shouldStopExecution(1)) {
+      break;
+    }
+    var divTile = document.createElement('div');
+    divTile.classList.add("tiles");
+    divTile.innerText = i;
+    divTile.id = i;
+    divTile.onmouseover = paintBox;
+    divGridWrapper.appendChild(divTile);
+  }
+  window.CP.exitedLoop(1);
 }
+
+// Paints each box inside the grid with a randomly created RGB value
 function paintBox() {
-    var r = Math.floor(Math.random() * 256);
-    var g = Math.floor(Math.random() * 256);
-    var b = Math.floor(Math.random() * 256);
-    var randomRGB = 'rgb(' + r + ',' + g + ',' + b + ')';
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
     this.innerText = "";
     this.innerHTML = '<span>rgb(</span>' + r + ', ' + g + ', ' + b + '<span>)</span>';
-    this.style.background = randomRGB;
+    this.style.background = 'rgb(' + r + ',' + g + ',' + b + ')';
     this.onclick = function () {
         document.execCommand("copy");
     };
@@ -35,12 +43,13 @@ function paintBox() {
     });
     this.onmouseout = removeSettings;
 }
+
+// Removes any existing settings so that a new color can be painted in the box
 function removeSettings() {
     this.innerText = this.id;
     if (this.id % 2 == 0) {
-        this.style.background = '#FFF';
-    }
-    else {
-        this.style.background = '#ECEFF1';
+      this.style.background = '#FFF';
+    } else {
+      this.style.background = '#ECEFF1';
     }
 }
